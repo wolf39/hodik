4bd83bef662a585d480e1a492e902fc87eb9d9f8
user <user@k2-r311-c06>
2014-04-30 12:20:54 +0400

    Project Created

---
+++ HodikProject/src/hodikproject/HodikProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hodikproject;
+
+/**
+ *
+ * @author user
+ */
+public class HodikProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
